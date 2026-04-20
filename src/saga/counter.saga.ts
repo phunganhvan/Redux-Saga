950fcd8362dab02f1bdde488a7f82959f0ad65b9
phung anh van <phunganhvan13122004@gmail.com>
2026-04-20 11:15:19 +0700

    action not hash code

--- src/saga/counter.saga.ts
+++ src/saga/counter.saga.ts
@@ -1,19 +1,14 @@
 import { put, takeLatest, delay } from "redux-saga/effects";
+import { incrementSagaFinish, decrementSagaFinish } from "../redux/counter/counter.slide";
 function* handleIncrease (action?: string){
     console.log("sagaFunction is running", action);
     yield delay(3000);
-    yield put ({
-        type: "counter/incrementSagaFinish",
-        payload: 2
-    })
+    yield put(incrementSagaFinish({value: 2}))
 }
 function* handleDecrease (action?: string){
     console.log("sagaFunction is running", action);
     yield delay(3000);
-    yield put ({
-        type: "counter/decrementSagaFinish",
-        payload: 2
-    })
+    yield put(decrementSagaFinish({value: 2}))
 }
 
 function* counterSaga() {
